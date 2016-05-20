54437935ea78731f5c93cc28d538686e9f985ed0
verstandinvictus <peter.k.cawley@gmail.com>
2016-05-19 18:20:38 -0700

    Initial commit. Reads a directory and spits out a list of files.

---
+++ SearchSandbox/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows;   
+
+namespace SearchSandbox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void DirectoryButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new System.Windows.Forms.FolderBrowserDialog();
+            var dialogResult = dialog.ShowDialog();
+            DirectoryButton.Visibility = Visibility.Collapsed;
+            Application.Current.Properties["DirectoryList"] = System.IO.Directory.GetFiles(dialog.SelectedPath);
+            string result = string.Join(", ", (string[])Application.Current.Properties["DirectoryList"]);
+            MessageBox.Show(result);
+        }
+    }
+}
